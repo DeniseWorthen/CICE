c61262f2e37cffe08d12111a809bbef3f75f0e2b
Denise Worthen <denise.worthen@noaa.gov>
2025-07-23 15:31:48 -0400

    add ijb track

--- cicecore/cicedyn/general/ice_step_mod.F90
+++ cicecore/cicedyn/general/ice_step_mod.F90
@@ -624,6 +624,9 @@
       use ice_state, only: aice, aicen, aice0, trcr_depend, &
           aicen_init, vicen_init, trcrn, vicen, vsnon, &
           trcr_base, n_trcr_strata, nt_strata
+      ! debug
+      use ice_communicate, only : my_task
+      use icepack_therm_shared, only : ijb
 
       real (kind=dbl_kind), intent(in) :: &
          dt      ! time step
@@ -663,6 +666,7 @@
          nltrcr = 0
       endif
 
+      ijb = 0
       this_block = get_block(blocks_ice(iblk),iblk)
       ilo = this_block%ilo
       ihi = this_block%ihi
@@ -671,7 +675,10 @@
 
       do j = jlo, jhi
       do i = ilo, ihi
-
+         ijb(1) = this_block%i_glob(i)
+         ijb(2) = this_block%j_glob(j)
+         ijb(3) = iblk
+         !print *,'XXX ',my_task,ijb
          if (tmask(i,j,iblk)) then
 
          ! significant wave height for FSD
